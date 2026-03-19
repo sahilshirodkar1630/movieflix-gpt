cbb30b2a208efcaadb6db901f8c0420cf27b2e31
Sahil Shirodkar <sahilshirodkar@1630@gmail.com>
2026-03-20 02:52:18 +0530

    fixed firebase bug

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -3,40 +3,39 @@ import { API_OPTIONS } from "../utils/constants";
 import { useEffect } from "react";
 import { addTrailerVideos } from "../utils/moviesSlice";
 
-  const useMovieTrailer = (movieId) => {
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+  const trailerVideos = useSelector((store) => store.movies.trailerVideos);
 
-    const dispatch = useDispatch();
-    const trailerVideos = useSelector(store => store.movies.trailerVideos);
-    useEffect(()=>{
-       if(!movieId) return
-       if (trailerVideos[movieId]) return;
+  useEffect(() => {
+    if (!movieId) return;
+    if (trailerVideos[movieId]) return;
 
-      const fetchTrailer = async () => {
+    const fetchTrailer = async () => {
       const data = await fetch(
         `https://api.themoviedb.org/3/movie/${movieId}/videos`,
         API_OPTIONS
       );
       const json = await data.json();
 
-      console.log("Videos: ", data);
-      if(!json.results){
-         return;
-      }
-     
-      const filteredData = json.results?.filter(
-         (video) => video.type === "Trailer" && video.name === "Official Trailer"
+      if (!json.results) return;
+
+      const filteredData = json.results.filter(
+        (video) =>
+          video.type === "Trailer" &&
+          video.name === "Official Trailer"
       );
-         
-      const trailer = filteredData?.length>0 ? filteredData[0] : json?.results[0];
-      console.log("Trailer: ", trailer);
+
+      const trailer =
+        filteredData.length > 0
+          ? filteredData[0]
+          : json.results[0];
+
       dispatch(addTrailerVideos({ movieId, trailer }));
     };
 
     fetchTrailer();
-      
-    },[movieId])
+  }, [movieId, trailerVideos, dispatch]);
+};
 
-  }
-  
-  export default useMovieTrailer;
-
+export default useMovieTrailer;
